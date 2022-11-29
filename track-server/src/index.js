be1619db8e91b354481ac74fec41be296ad2d035
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-11-29 10:03:16 -0800

    env and mongo set up

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
+const MONGO_PW = process.env.MONGO_PW;
+
+const mongoUri = `mongodb+srv://mattchiz:${MONGO_PW}@cluster0.mjgyjrb.mongodb.net/?retryWrites=true&w=majority`;
 
 app.get("/", (req, res) => {
   res.send("Hi there!");
