bbd31551d0146295aee0af6b1eee468109175822
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-11-29 10:07:26 -0800

    nodemon

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -10,6 +10,9 @@ mongoose.connect(mongoUri);
 mongoose.connection.on("connected", () => {
   console.log("Connected to mongo instance");
 });
+mongoose.connection.on("error", (err) => {
+  console.error("Error connecting to mongo", err);
+});
 
 app.get("/", (req, res) => {
   res.send("Hi there!");
