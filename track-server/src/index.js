34400d648b06c65dbddc68e7c8594bc6aba82ead
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-11-29 10:05:09 -0800

    mongo connected

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
-const MONGO_PW = process.env.MONGO_PW;
 
+const MONGO_PW = process.env.MONGO_PW;
 const mongoUri = `mongodb+srv://mattchiz:${MONGO_PW}@cluster0.mjgyjrb.mongodb.net/?retryWrites=true&w=majority`;
+mongoose.connect(mongoUri);
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance");
+});
 
 app.get("/", (req, res) => {
   res.send("Hi there!");
