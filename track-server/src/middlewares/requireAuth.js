eb0ff04cd074a51f91385f8bb51d91916f0fce25
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-12-04 10:02:51 -0800

    second check and finding user

--- track-server/src/middlewares/requireAuth.js
+++ track-server/src/middlewares/requireAuth.js
@@ -1,11 +1,26 @@
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
+require("dotenv").config();
+
+const key = process.env.JWT_SECRET;
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
+  /// authorization === 'Bearer sdksdfhkshdfd'
   if (!authorization) {
     return res.status(401).send({ error: "You must be logged in." });
   }
+
+  const token = authorization.replace("Bearer ", "");
+  jwt.verify(token, key, async (err, payload) => {
+    if (err) {
+      return res.status(401).send({ error: "You must be logged in." });
+    }
+
+    const { userId } = payload;
+
+    const user = await User.findById(userId);
+  });
 };
