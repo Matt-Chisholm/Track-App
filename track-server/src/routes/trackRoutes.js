1c015d5db714e845fa466c35ad0b9c21e7637ec3
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-12-05 09:50:54 -0800

    starting track routes

---
+++ track-server/src/routes/trackRoutes.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const requireAuth = require("../middlewares/requireAuth");
+const Track = mongoose.model("Track");
+
+const router = express.Router();
