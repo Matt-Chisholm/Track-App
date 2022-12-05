e5f1d637f9e26a9efd78f29613510ca49883707d
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-12-05 12:32:15 -0800

    state hooked up to inputs

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -1,17 +1,31 @@
 import { View, StyleSheet } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { Text, Input, Button } from "react-native-elements";
 import Spacer from "../components/Spacer";
 
 export default function SignupScreen({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <View style={styles.container}>
       <Spacer>
         <Text h3>Sign Up for Tracker</Text>
       </Spacer>
-      <Input label='Email' />
+      <Input
+        label='Email'
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize='none'
+        autoCorrect={false}
+      />
       <Spacer />
-      <Input label='Password' />
+      <Input
+        secureTextEntry
+        label='Password'
+        value={password}
+        onChangeText={setPassword}
+      />
       <Spacer>
         <Button title='Sign Up' />
       </Spacer>
