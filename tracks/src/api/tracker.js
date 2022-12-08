2d5599709159c3b5f88e138b0f95e559f9b379c9
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-12-08 10:38:26 -0800

    nav working

--- tracks/src/api/tracker.js
+++ tracks/src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://04a9-2001-569-bd71-a100-b128-6d6d-d8a1-c0ae.ngrok.io",
+  baseURL: "https://4018-2001-569-bd71-a100-c015-4a83-afb1-71c4.ngrok.io",
 });
