f331cca518840b528faddb19c84af7996afcfc07
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-12-09 11:25:03 -0800

    signinscreen set up

--- tracks/src/screens/SigninScreen.js
+++ tracks/src/screens/SigninScreen.js
@@ -1,12 +1,38 @@
-import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import { View, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import AuthForm from "../components/AuthForm";
+import NavLink from "../components/NavLink";
 
 export default function SigninScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <View>
-      <Text style={{ fontSize: 48 }}>SigninScreen</Text>
+    <View style={styles.container}>
+      <AuthForm
+        headerText='Sign In to Your Account'
+        errorMessage=''
+        submitButtonText='Sign In'
+        onSubmit={() => {}}
+      />
+      <NavLink
+        routeName='Signup'
+        text="Don't have an account? Sign up instead"
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+SigninScreen.navigationOptions = () => {
+  return {
+    header: () => false,
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 250,
+  },
+});
