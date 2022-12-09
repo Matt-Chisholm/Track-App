7fbd2c18d0a2895002fec6c10388d8934aaca166
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-12-09 13:05:24 -0800

    resolveAuthScreen

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -1,15 +1,17 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { View, StyleSheet } from "react-native";
-import {
-  Context as AuthContext,
-  clearErrorMessage,
-} from "../context/AuthContext";
+import { Context as AuthContext } from "../context/AuthContext";
 import AuthForm from "../components/AuthForm";
 import NavLink from "../components/NavLink";
 import { NavigationEvents } from "react-navigation";
 
 const SignupScreen = ({ navigation }) => {
-  const { state, signup } = useContext(AuthContext);
+  const { state, signup, clearErrorMessage, tryLocalSignin } =
+    useContext(AuthContext);
+
+  useEffect(() => {
+    tryLocalSignin();
+  }, []);
 
   return (
     <View style={styles.container}>
