cd38543974ea695a9c44a50c8c4d03e2863e501e
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-12-09 10:11:09 -0800

    reuseable auth form

---
+++ tracks/src/components/AuthForm.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text, Button, Input, StyleSheet } from "react-native-elements";
+
+const AuthForm = () => {};
+
+const styles = StyleSheet.create({});
+
+export default AuthForm;
