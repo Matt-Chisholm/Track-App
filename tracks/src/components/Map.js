ce0560c5eeb6b1a78647923faeb6f6a723ce2833
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-12-11 13:29:45 -0800

    initial region

--- tracks/src/components/Map.js
+++ tracks/src/components/Map.js
@@ -3,7 +3,17 @@ import React from "react";
 import MapView from "react-native-maps";
 
 export default function Map() {
-  return <MapView style={styles.map} />;
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={{
+        latitude: 37.33233,
+        longitude: -122.03121,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      }}
+    />
+  );
 }
 
 const styles = StyleSheet.create({
