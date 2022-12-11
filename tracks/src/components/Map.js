0c5f531549405315ca61dcea5cb8a20906afbab9
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-12-11 13:34:39 -0800

    playing with points

--- tracks/src/components/Map.js
+++ tracks/src/components/Map.js
@@ -1,8 +1,22 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
-import MapView from "react-native-maps";
+import MapView, { Polyline } from "react-native-maps";
 
 export default function Map() {
+  let points = [];
+  for (let i = 0; i < 20; i++) {
+    if (i % 2 === 0) {
+      points.push({
+        latitude: 37.33233 + i * 0.0002,
+        longitude: -122.03121 + i * 0.0003,
+      });
+    } else {
+      points.push({
+        latitude: 37.33233 - i * 0.0002,
+        longitude: -122.03121 - i * 0.0005,
+      });
+    }
+  }
   return (
     <MapView
       style={styles.map}
@@ -11,8 +25,9 @@ export default function Map() {
         longitude: -122.03121,
         latitudeDelta: 0.01,
         longitudeDelta: 0.01,
-      }}
-    />
+      }}>
+      <Polyline coordinates={points} />
+    </MapView>
   );
 }
 
