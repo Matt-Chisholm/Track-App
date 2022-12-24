058df52b0612aae6189ea3bb8b3a7716f6ee0318
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-12-24 09:28:18 -0800

    new ngrok

--- tracks/src/api/tracker.js
+++ tracks/src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://93bf-2001-569-bd71-a100-5f6-895-450a-9115.ngrok.io",
+  baseURL: "https://d162-2001-569-bd71-a100-5f6-895-450a-9115.ngrok.io",
 });
