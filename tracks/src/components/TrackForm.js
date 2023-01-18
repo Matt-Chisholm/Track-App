631cce7a7d52d75560abef74c099c5035b52b94c
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-01-18 11:39:56 -0800

    spacer

--- tracks/src/components/TrackForm.js
+++ tracks/src/components/TrackForm.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Input, Button } from "react-native-elements";
+import Spacer from "./Spacer";
 
 const TrackForm = () => {
   return (
     <>
-      <Input placeholder='Enter name' />
+      <Spacer>
+        <Input placeholder='Enter name' />
+      </Spacer>
       <Button title='Start Recording' />
     </>
   );
