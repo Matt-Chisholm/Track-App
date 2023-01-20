0c2ffdc740b71a2749fe612dcf084f6acb25d76b
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-01-20 09:52:10 -0800

    fix callback bug with useCallback

--- tracks/src/screens/TrackCreateScreen.js
+++ tracks/src/screens/TrackCreateScreen.js
@@ -2,7 +2,7 @@ import "../_mockLocation";
 import { StyleSheet } from "react-native";
 import { Text } from "react-native-elements";
 import { SafeAreaView, withNavigationFocus } from "react-navigation";
-import React, { useContext } from "react";
+import React, { useContext, useCallback } from "react";
 import Map from "../components/Map";
 import { Context as LocationContext } from "../context/LocationContext";
 import useLocation from "../hooks/useLocation";
@@ -10,9 +10,13 @@ import TrackForm from "../components/TrackForm";
 
 const TrackCreateScreen = ({ isFocused }) => {
   const { state, addLocation } = useContext(LocationContext);
-  const [err] = useLocation(isFocused, (location) => {
-    addLocation(location, state.recording);
-  });
+  const callback = useCallback(
+    (location) => {
+      addLocation(location, state.recording);
+    },
+    [state.recording]
+  );
+  const [err] = useLocation(isFocused || state.recording, callback);
 
   return (
     <SafeAreaView forceInset={{ top: "always" }}>
