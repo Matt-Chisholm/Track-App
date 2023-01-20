e100ec29384839f41a078f612feedcc635c545da
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-01-20 10:27:20 -0800

    startwatching in useEffect

--- tracks/src/hooks/useLocation.js
+++ tracks/src/hooks/useLocation.js
@@ -9,27 +9,26 @@ export default (shouldTrack, callback) => {
   const [err, setErr] = useState(null);
   const [subscriber, setSubscriber] = useState(null);
 
-  const startWatching = async () => {
-    try {
-      const { granted } = await requestForegroundPermissionsAsync();
-      if (!granted) {
-        throw new Error("Location permission not granted");
-      }
-      const sub = await watchPositionAsync(
-        {
-          accuracy: Accuracy.BestForNavigation,
-          timeInterval: 1000,
-          distanceInterval: 10,
-        },
-        callback
-      );
-      setSubscriber(sub);
-    } catch (e) {
-      setErr(e);
-    }
-  };
-
   useEffect(() => {
+    const startWatching = async () => {
+      try {
+        const { granted } = await requestForegroundPermissionsAsync();
+        if (!granted) {
+          throw new Error("Location permission not granted");
+        }
+        const sub = await watchPositionAsync(
+          {
+            accuracy: Accuracy.BestForNavigation,
+            timeInterval: 1000,
+            distanceInterval: 10,
+          },
+          callback
+        );
+        setSubscriber(sub);
+      } catch (e) {
+        setErr(e);
+      }
+    };
     if (shouldTrack) {
       startWatching();
     } else {
